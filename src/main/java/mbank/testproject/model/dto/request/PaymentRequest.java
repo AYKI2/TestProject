6347b06499f68e8e8a1aca77dc727607b74868f0
AYKI2 <abdulhamitov.bstn@gmail.com>
2025-01-09 21:29:48 +0600

    Added PaymentController, GlobalExceptionHandler

--- src/main/java/mbank/testproject/model/dto/request/PaymentRequest.java
+++ src/main/java/mbank/testproject/model/dto/request/PaymentRequest.java
@@ -1,7 +1,10 @@
 package mbank.testproject.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import java.math.BigDecimal;
 
+@JsonRootName("paymentRequest")
 public record PaymentRequest(
         String statusType,
         BigDecimal amount,
